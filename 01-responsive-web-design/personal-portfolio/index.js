de709914ea33eb93f6a134e321c8df247e344800
antalmega <kikoalmelagarcia@gmail.com>
2018-10-31 18:59:37 +0100

    change prettier setting

--- 01-responsive-web-design/personal-portfolio/index.js
+++ 01-responsive-web-design/personal-portfolio/index.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 window.onload = function() {
   // On portrait view, Collapse deployed menu when menu item is clicked
-  const checkbox = document.getElementById("navi-toggle");
+  const checkbox = document.getElementById('navi-toggle');
   const uncheck = () => (checkbox.checked = false);
-  const menuItems = document.querySelectorAll(".navbar__link");
-  menuItems.forEach(el => el.addEventListener("click", uncheck));
+  const menuItems = document.querySelectorAll('.navbar__link');
+  menuItems.forEach(el => el.addEventListener('click', uncheck));
 
   // On portrait view, Collapse menu when deployed and logo is clicked
-  const navLogo = document.querySelector(".navbar__logo-img");
-  navLogo.addEventListener("click", () => {
+  const navLogo = document.querySelector('.navbar__logo-img');
+  navLogo.addEventListener('click', () => {
     if (checkbox.checked) uncheck();
   });
 
@@ -17,7 +17,7 @@ window.onload = function() {
     // get the viewport height and multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
     // set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
 
   setVhVariable();
@@ -29,38 +29,38 @@ window.onload = function() {
 
   //  highlight menu item related to the current section on viewport
   function highlightMenuBySection() {
-    const sections = document.querySelectorAll("section");
-    let navHeight = document.getElementById("navbar").offsetHeight;
-    let headerHeight = document.querySelector("header").offsetHeight;
+    const sections = document.querySelectorAll('section');
+    let navHeight = document.getElementById('navbar').offsetHeight;
+    let headerHeight = document.querySelector('header').offsetHeight;
     let currentPos = window.scrollY;
 
     sections.forEach(section => {
       let top = section.offsetTop - navHeight;
       let bottom = top + section.offsetHeight;
-      let href = `#${section.getAttribute("id")}`;
+      let href = `#${section.getAttribute('id')}`;
 
       if (currentPos >= top && currentPos <= bottom) {
         menuItems.forEach(item => {
-          let itemHref = item.getAttribute("href");
+          let itemHref = item.getAttribute('href');
           itemHref === href
-            ? item.classList.add("active")
-            : item.classList.remove("active");
+            ? item.classList.add('active')
+            : item.classList.remove('active');
         });
       } else if (currentPos < headerHeight - navHeight) {
-        menuItems.forEach(item => item.classList.remove("active"));
+        menuItems.forEach(item => item.classList.remove('active'));
       }
     });
   }
 
   // clean and update active class while mouse over menu
-  document.querySelectorAll(".navbar__link").forEach(link => {
-    link.addEventListener("mouseenter", function() {
-      menuItems.forEach(item => item.classList.remove("active"));
+  document.querySelectorAll('.navbar__link').forEach(link => {
+    link.addEventListener('mouseenter', function() {
+      menuItems.forEach(item => item.classList.remove('active'));
     });
   });
 
-  document.querySelectorAll(".navbar__link").forEach(link => {
-    link.addEventListener("mouseleave", highlightMenuBySection);
+  document.querySelectorAll('.navbar__link').forEach(link => {
+    link.addEventListener('mouseleave', highlightMenuBySection);
   });
 
   /* 
